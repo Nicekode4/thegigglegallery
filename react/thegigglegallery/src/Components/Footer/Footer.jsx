5030d93afdb3e689eed31ce7b3acaf01dbbcf850
Philip Thomsen <philip-th@live.dk>
2023-06-22 09:14:21 +0200

    Update Footer.jsx

--- react/thegigglegallery/src/Components/Footer/Footer.jsx
+++ react/thegigglegallery/src/Components/Footer/Footer.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+    <p>Ⓒ2022 TheGiggleGallery</p>
+          <p>CVR: 12345678</p>
+          <p>Adressevej 123</p>
+          <p>1234 Bynavnhavn</p>
+          <p>+45 1234 5678</p>
+          <p>info@mychoiceshop.dk</p>
+          <p>Find os her:</p>
+          <p>Ⓒ✔ in</p>
+          <p>Sikker betaling:</p>
+          <p>VISA VISA
+          MobilePay Masky Gard</p>
+          <p>Alle rettigheder forbeholdes</p>
+          <p>Bemærk!</p>
+          <p>Der er først indgået en bindende aftale, når
+          vi har bekræftet din bestilling.</p>
+      </div>
   )
 }
 
