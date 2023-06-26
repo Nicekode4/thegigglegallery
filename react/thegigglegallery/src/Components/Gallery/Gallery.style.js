9926cf5b2908210b5d3f2f4f4c2ec010ca648ddb
Philip Thomsen <philip-th@live.dk>
2023-06-26 09:53:01 +0200

    Update

--- react/thegigglegallery/src/Components/Gallery/Gallery.style.js
+++ react/thegigglegallery/src/Components/Gallery/Gallery.style.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 
 export const GalleryStyle = styled.article`
+.gallery {
+    display: flex;
+    justify-content: center;
+}
+
+.border {
+    display: flex;
+    padding: 20px;
+    border: 3px solid #262234;
+    border-radius: 5px;
+    background-color: white;
+    filter: drop-shadow(5px 5px 5px gray);
+}
+
+.arrow {
+    border: none;
+    font-size: 50px;
+    background-color: white;
+    color: #c69a6b;
+}
+
 .image{
-    height: 10vh;
+    height: 30vh;
 }
 `
