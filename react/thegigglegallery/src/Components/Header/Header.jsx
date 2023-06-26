4d3f36d046a9a1c5add5edb20898627a0551f2ff
Philip Thomsen <philip-th@live.dk>
2023-06-26 12:16:44 +0200

    Update

--- react/thegigglegallery/src/Components/Header/Header.jsx
+++ react/thegigglegallery/src/Components/Header/Header.jsx
@@ -5,13 +5,12 @@ import SidebarDrawer from '../SidebarDrawer/SidebarDrawer'
 function Header() {
 	return (
 		<HeaderStyle>
-      <ul>
-        <li></li>
-      </ul>
 			<div className="top">
         <SidebarDrawer />
 				<h1><span className="s1">The</span> Gi<span className="s2">gg</span>le Ga<span className="s3">ll</span>ery</h1>
-			</div>
+			
+      </div>
+      
 		</HeaderStyle>
 	
 	)
---
+++ react/thegigglegallery/src/Pages/ProductPage/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ProductPageStyle } from './ProductPage.style'
+
+function ProductPage() {
+  return (
+    <ProductPageStyle>
+    <div>ProductPage</div>
+    </ProductPageStyle>
+  )
+}
+
+export default ProductPage
