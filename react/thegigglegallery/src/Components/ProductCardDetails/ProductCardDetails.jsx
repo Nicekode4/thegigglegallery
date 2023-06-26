c435a2d1eda904de8acc3825ad01214471448bb3
Philip Thomsen <philip-th@live.dk>
2023-06-26 13:56:30 +0200

    update

--- react/thegigglegallery/src/Components/ProductCardDetails/ProductCardDetails.jsx
+++ react/thegigglegallery/src/Components/ProductCardDetails/ProductCardDetails.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { ProductCardDetailsStyle } from './ProductCardDetails.style'
+import placeholder from '../../assets/images/placeholder.png'
 
 function ProductCardDetails(props) {
   return (
     <ProductCardDetailsStyle>
         <img src={props.img} alt="product image" className='productImg'/>
         <p className='title'>{props.title}</p>
-        <p className='desc'>{props.desc}</p>
-        <p className='price'>Kr {props.price}</p>
+        <div className='ratingDiv'>
+          <h6>4.7 <p>(212 Annmeldelser)</p></h6>
+          <p className='desc'>{props.desc}</p>
+          <div>
+            <p className='price'>Kr {props.price}</p>
+            <div>
+              <button>Læg i kurv</button>
+              <p>På lager: {props.lager}</p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h2>Anmeldelser</h2>
+            <button>Skriv en anmeldelse</button>
+          </div>
+          <h3>Anmdelser</h3>
+          <hr />
+          <h2>Ingen anmelder endnu..</h2>
+        </div>
     </ProductCardDetailsStyle>
   )
 }
--- react/thegigglegallery/src/Pages/ProductPage/ProductPage.jsx
+++ react/thegigglegallery/src/Pages/ProductPage/ProductPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { ProductPageStyle } from './ProductPage.style'
+import ProductCardDetails from '../../Components/ProductCardDetails/ProductCardDetails.jsx'
+import placeholder from '../../assets/images/placeholder.png'
 
 function ProductPage() {
   return (
     <ProductPageStyle>
-    <div>ProductPage</div>
+    <ProductCardDetails 
+    title="lel"
+    desc="lel"
+    price="lel"
+    img={placeholder}
+    lager={2}
+    />
+    
     </ProductPageStyle>
   )
 }
